472b8c774a893d8951cda5a8f8ce254d09313e1e
JLYJ <jlyj@ase.dk>
2017-12-09 09:04:49 +0100

    # less ambitious testfunc

--- tests/test_fars_read.R
+++ tests/test_fars_read.R
@@ -6,13 +6,6 @@
 library(testthat)
 
 # change dir to load fresh batch of data
-setwd(system.file("inst/extdata", package = "farsfunctions"))
-filename <- make_filename(2014)
-my_data <- fars_read(filename)
 
-# change dir to load saved image of data, when the function were
-setwd(system.file("tests/", package = "farsfunctions"))
-compare <- readRDS("fars_read_data_04122017.RDS")
-
-# we expect these objects to be identical
-testthat::expect_equal(compare, my_data)
+# we expect an error when we pick an invalid state number
+testthat::expect_error(fars_map_state(100,2013))
