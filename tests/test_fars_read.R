7b37d3736943cb166a6a6931de5ad7748dc953c9
JLYJ <jlyj@ase.dk>
2017-12-09 08:57:53 +0100

    # removed devtools:document()

--- tests/test_fars_read.R
+++ tests/test_fars_read.R
@@ -1,11 +1,18 @@
 
 
 # This function tests, by comparing to a saved image of correctly loaded data
+
+# source
 library(testthat)
-devtools::document()
+
+# change dir to load fresh batch of data
 setwd(system.file("inst/extdata", package = "farsfunctions"))
 filename <- make_filename(2014)
 my_data <- fars_read(filename)
 
-compare <- readRDS("../../tests/fars_read_data_04122017.RDS")
+# change dir to load saved image of data, when the function were
+setwd(system.file("tests/", package = "farsfunctions"))
+compare <- readRDS("fars_read_data_04122017.RDS")
+
+# we expect these objects to be identical
 testthat::expect_equal(compare, my_data)
